eb2f1520b749ef442d5eb5e68806fcc12efddf48
robsonjerlley <robsonjerlley.work@gmail.com>
2025-10-21 18:39:38 -0300

    Adicionado dependencia do Swageer

--- academia-digital-master/academia-digital/src/main/java/me/dio/academia/digital/config/SwaggerConfig.java
+++ academia-digital-master/academia-digital/src/main/java/me/dio/academia/digital/config/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package me.dio.academia.digital.config;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
