3e1550725c6cfc37f133bacb932d88264759fa45
robsonjerlley <robsonjerlley.work@gmail.com>
2025-10-23 18:31:28 -0300

    Iniciando criação de testes unitarios.

---
+++ academia-digital-master/academia-digital/src/test/java/me/dio/academia/digital/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,12 @@
+package me.dio.academia.digital.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutenticacaoControllerTest {
+
+    @Test
+    void autenticar() {
+    }
+}
---
+++ academia-digital-master/academia-digital/src/test/java/me/dio/academia/digital/controller/UsuarioControllerTest.java
@@ -0,0 +1,20 @@
+package me.dio.academia.digital.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
